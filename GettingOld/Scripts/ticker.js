f4af4f0b4b6352dbf25e5906469b747c378c8e54
aphramzo <im.t.wal@gmail.com>
2015-02-07 13:55:39 -0700

    Countdowns now ticking down not up

--- GettingOld/Scripts/ticker.js
+++ GettingOld/Scripts/ticker.js
@@ -1,13 +1,28 @@
 ﻿$(function () {
-    var tick = function ($container) {
-        $container.html((parseInt($container.html().replace(/,/g, '')) + 1).toLocaleString('en-US'));
+    var tick = function ($container, countUp) {
+        var value = parseInt($container.html().replace(/,/g, ''));
+        if (countUp) {
+            value = (value + 1)
+        }
+        else {
+            value = (value - 1)
+        }
+        $container.html(value.toLocaleString('en-US'));
     };
 
     $.each($('span.seconds'), function (key, value) {        
-        window.setInterval(tick, 1000, $(value));
+        window.setInterval(tick, 1000, $(value), true);
     });
 
     $.each($('span.minutes'), function (key, value) {
-        window.setInterval(tick, 60000, $(value));
+        window.setInterval(tick, 60000, $(value), true);
+    });
+
+    $.each($('span.seconds-down'), function (key, value) {
+        window.setInterval(tick, 1000, $(value), false);
+    });
+
+    $.each($('span.minutes-down'), function (key, value) {
+        window.setInterval(tick, 60000, $(value), false);
     });
 });
